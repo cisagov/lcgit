7360374e78661d8dfa5ec550337a4e398c3bb4e8
Felddy <markf+github@geekpad.com>
2019-05-06 15:11:45 -0400

    Improve version handling

--- src/lcgit/__init__.py
+++ src/lcgit/__init__.py
@@ -4,6 +4,6 @@ This library is used to generate randomized sequences from python sequences
 and IP networks.
 """
 from .lcgit import lcg
+from ._version import __version__  # noqa: F401
 
 __all__ = ["lcg"]
-__version__ = "0.0.1"

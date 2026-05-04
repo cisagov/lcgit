0822ba625b3dc9d90e1032c5c64a1c1995a85215
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-04 16:27:31 -0400

    Prepend unused loop variable with an underscore
    This gets rid of an error from flake8.

--- tests/test_lcgit.py
+++ tests/test_lcgit.py
@@ -50,7 +50,7 @@ def test_state_save_and_restore(sequence):
     break_at = len(lcg1) / 2
     count = 0
     state = None
-    for i, state in lcg1:
+    for i, _state in lcg1:
         count += 1
         accumulated.append(i)
         if count == break_at:
